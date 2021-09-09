5a0a2ea8bba783d403d92273c91a7a1052166014
Reem <reemalgabani@gmail.com>
2021-09-09 17:20:20 +0300

    pod

--- nahaj/lib/main.dart
+++ nahaj/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
