523ce113174e8418185ca7b5a5ff1ce413eac667
TaifAldehbash <438201108@student.ksu.edu.sa>
2021-09-17 18:13:23 +0300

    #6.2.0 added the home page and assets folder

---
+++ nahaj/lib/homePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    //background
+    return Scaffold();
+  }
+}
