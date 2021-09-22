8baeb128e67ff1a73be973959f84f739cc6b0022
TaifAldehbash <438201108@student.ksu.edu.sa>
2021-09-22 12:19:52 +0300

    change dimension to landscape

--- nahaj/lib/main.dart
+++ nahaj/lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:nahaj/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  /*SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);*/
   await Firebase.initializeApp();
   runApp(MyApp());
 }
