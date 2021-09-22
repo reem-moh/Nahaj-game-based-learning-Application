a67618cc2d095643d7052f83e9cfcbebf119021f
Reem <reemalgabani@gmail.com>
2021-09-22 20:22:40 +0300

    #6.2.0 enable landscape Mode only

--- nahaj/lib/main.dart
+++ nahaj/lib/main.dart
@@ -3,11 +3,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:nahaj/homepage.dart';
 import 'package:nahaj/database.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   DataBase db=DataBase();
   runApp(MyApp(db));
 }
