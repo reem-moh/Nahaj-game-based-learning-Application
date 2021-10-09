f3e0e73cc5f4fc5b1864dcf99ba0693f0fde8bc6
TaifAldehbash <438201108@student.ksu.edu.sa>
2021-10-09 17:07:46 +0300

    potions added

---
+++ Experiments/Assets/Experiments/Volcano/2Ginge/LiquidSimulation/Scripts/PourLiquid.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourLiquid : MonoBehaviour {
+
+	// Use this for initialization
+    //how quickly it takes to empty
+    public float rateOfFlow = 1.0f;
+    //such b....stuff
+    public BottleSmash smashScript;
+    public LiquidVolumeAnimator liquid;
+    public Transform controllingTransform;
+    public ParticleSystem pouringParticleSystem;
+    //how many particles it takes to empty
+    public float volumeOfParticles = 70.0f;
+    private Rigidbody corkRB;
+	void Start () {
+        if (smashScript != null)
+        {
+            if (smashScript.Cork != null)
+            {
+                corkRB = smashScript.Cork.GetComponent<Rigidbody>();
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(corkRB != null)
+        {
+            if (corkRB.isKinematic)
+                return;
+            else if (!corkRB.gameObject.activeInHierarchy)
+                return;
+        }
+
+        float d = Vector3.Dot(controllingTransform.up, (liquid.finalPoint  - liquid.finalAnchor).normalized);
+        float d2 = (d + 1.0f) / 2;
+        float particleVal = 0.0f;
+        if(d2 < liquid.level)
+        {
+            particleVal = (liquid.level - d2) * rateOfFlow;
+            liquid.level = Mathf.Lerp(liquid.level, d2, Time.deltaTime * rateOfFlow);
+        }
+        if(d <= 0.0f)
+        {
+            if(liquid.level > float.Epsilon)
+                particleVal = liquid.level;
+            liquid.level = Mathf.Lerp(liquid.level, 0, Time.deltaTime * rateOfFlow);
+        }
+        if(pouringParticleSystem != null)
+        {
+            ParticleSystem.MinMaxCurve emi = pouringParticleSystem.emission.rateOverTime;
+            emi.constant = volumeOfParticles * particleVal;
+            ParticleSystem.EmissionModule emod = pouringParticleSystem.emission;
+            emod.rateOverTime = emi;
+            //pouringParticleSystem.emission = emod;
+            if(particleVal > 0)
+            {
+                if(!pouringParticleSystem.isEmitting)
+                {
+                    pouringParticleSystem.Play();
+                }
+            }
+            else
+            {
+                pouringParticleSystem.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+            }
+        }
+		
+	}
+}
---
+++ Experiments/Assets/Experiments/Volcano/2Ginge/MagicPotions/Documentation/ReadMe.txt
@@ -0,0 +1,17 @@
+Thank you for purchasing Potions with Liquid Simulation via the Unity Asset Store.
+
+Please read the PDF documentation provided for set up instructions, details regarding script functionality and best use guides, as well as FAQ and 
+version changes. 
+
+Demonstration and tutorial video content is available via our Youtube channel www.youtube.com/channel/UC2T7YzK-hq2gvIWJlIKfwug
+
+If you require additional support please send us and email to contact@2ginge.com
+
+V1.0 - June 2017
+V1.1 - July 2017
+V1.2 - October 2017
+V2.0 - November 2017
+V3.0 - July 2018
+
+
+© 2Ginge 2017-2018
