6bfe948f827f39b884e4fedb877735214748380a
ghaida <438202619@student.ksu.edu.sa>
2021-11-04 23:27:53 +0300

    UI chat

--- nahaj/lib/group.dart
+++ nahaj/lib/group.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'database.dart';
+import 'homePage.dart';
+import 'package:flutter/cupertino.dart';
+
 
 class Group extends StatefulWidget {
   //final DataBase db;
@@ -24,6 +27,7 @@ class _Group extends State<Group> {
   String username = "1";
   String uid = "0";
   bool entere = true;
+  int numOfchate=3 ;
 
   Future<void> _getInfoFromSession() async {
     final prefs = await SharedPreferences.getInstance();
@@ -65,19 +69,28 @@ if(entere){
                   ),
                   // ignore: deprecated_member_use
 
-                  TextButton(
+ TextButton(
                     child: Padding(
-                  padding: EdgeInsets.only(top: 0, right: 830),
-                  child: Image(
-                      image: AssetImage("assets/Group13.png"),
-                      alignment: Alignment.topLeft,
-                    ),
-                      ),
+                  padding: EdgeInsets.only(top: 20, right: 720),
+                  child: Padding(
+                  padding: EdgeInsets.only(top: 0, left: 0),
+                  child: Icon(Icons.arrow_back, size: size.height * 0.08),
+                      ),),
                     onPressed: () {
-// description of the group
-                    },
+                  setState(() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(
+                                db: widget.db,
+                              )),
+                    );
+                  });
+                },
+
                 
               ),
+                  
 
                   Container(
                     margin: EdgeInsets.only(right: 30.0),
@@ -109,13 +122,29 @@ if(entere){
                     ),
                   ),
   ),
+
+  TextButton(
+                    child: Padding(
+                  padding: EdgeInsets.only(top: 0, right: 50),
+                  child: Image(
+                      image: AssetImage("assets/Group13.png"),
+                      alignment: Alignment.topLeft,
+                    ),
+                      ),
+                    onPressed: () {
+// description of the group
+                    },
+                
+              ),
                 ],
               )
             ],
           ),
-
-
-
+ SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              physics: ClampingScrollPhysics(),
+              child:
+         //chat
           Stack(
             children: [
               Container(
@@ -126,12 +155,36 @@ if(entere){
                   color: Colors.white,
                 ),
               ),
+              ListView.builder(
+  itemCount: messages.length,
+  shrinkWrap: true,
+  padding: EdgeInsets.only(top: 10,bottom: 10),
+  physics: ClampingScrollPhysics(),
+  itemBuilder: (context, index){
+    return Container(
+      padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+      child: Align(
+        alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:Colors.blue[200]),
+          ),
+          padding: EdgeInsets.all(16),
+          child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
+        ),
+      ),
+    );
+  },
+),
+
               //list view
+              /*
               Container(
                 margin: EdgeInsets.only(left: 30.0, right: 30.0),
                 height: 280,
                 child: ListView.separated(
-                  itemCount: 3,
+                  itemCount: numOfchate,
                   separatorBuilder: (BuildContext context, int index) {
                     return SizedBox(
                       width: 25,
@@ -150,70 +203,93 @@ if(entere){
                   scrollDirection: Axis.vertical,
                 ),
               ),
+
+              */
             ],
           ),
+      ),
 
 
 
 
 
-          Stack(
-            children: [
-              
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Color.fromARGB(255, 224, 224, 224),
+          Padding(
+                              padding: EdgeInsets.only(top: 0),
+
+            child: Stack(
+              children: [
+                
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Color.fromARGB(255, 224, 224, 224),
+                  ),
                 ),
-              ),
-             Padding(
-                  padding: EdgeInsets.only(bottom: 0),
-                  child:
-              Row(
-                children: [
+               Padding(
+                    padding: EdgeInsets.only(bottom: 0),
+                    child:
+                Row(
+                  children: [
 
  Flexible(
-                  child: Column(
-                children: <Widget>[
-                  Container(
-                    child: TextField(
+                    child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: TextField(
+                            style: TextStyle(
+      fontSize: 30.0,
+      height: 2.0,
+      color: Colors.black                  
+    ),
+                        textDirection: TextDirection.rtl,
+
   decoration: InputDecoration(
    hintTextDirection: TextDirection.rtl,
-               hintText: " اكتب ... ",
-               
-               hintStyle: TextStyle(fontWeight: FontWeight.w300, color: Colors.black , fontSize: 30 )
-              ),  
-                      
-                    ),
-                  )
-                  //container
-                ],
-              )),
+                 hintText: " اكتب ... ",
+                 hintStyle: TextStyle(fontWeight: FontWeight.w300, color: Colors.black , fontSize: 30 )
+                 
+                ),  
+                        
+                      ),
+                    )
+                    //container
+                  ],
+                )),
 
      TextButton(
-                    child: Padding(
-                  padding: EdgeInsets.only(top: 0, left: 0),
-                  child: Icon(Icons.send, size: size.height * 0.1),
-                      ),
-                    onPressed: () {
+                      child: Padding(
+                    padding: EdgeInsets.only(top: 0, left: 0),
+                    child: Icon(Icons.send, size: size.height * 0.08),
+                        ),
+                      onPressed: () {
 // description of the group
-                    },
+                      },
+                  
+                ),
+
+
+                  ],
+                ),
+                ),
                 
-              ),
-
-
-                ],
-              ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 
+List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello", messageType: "receiver"),
+    ChatMessage(messageContent: "Hello", messageType: "receiver"),
+    ChatMessage(messageContent: "Hello", messageType: "sender"),
+    ChatMessage(messageContent: "Hello", messageType: "receiver"),
+    ChatMessage(messageContent: "IHello", messageType: "sender"),
+    
+  ];
   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 
   Future<void> getGroupName(String uid) async {
@@ -237,6 +313,11 @@ if(entere){
       }
     }
   }
+}
+class ChatMessage{
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
 }
 
 /*
