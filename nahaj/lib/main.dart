22523393c54f3be80f84c196e0a6197941b4da22
ghaida <438202619@student.ksu.edu.sa>
2021-11-04 15:07:40 +0300

    .

--- nahaj/lib/main.dart
+++ nahaj/lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AddGroup(
+      home: Signin(
         db: this.database,
       ),
       /*AddGroup2(
