fbe309142e75c17b78efd6a1470929407d6a877e
ghaida <438202619@student.ksu.edu.sa>
2021-11-09 09:24:49 +0300

    UI GroupInfo

---
+++ nahaj/lib/Group/GroupInfo.dart
@@ -0,0 +1,56 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nahaj/database.dart';
+
+class GroupInfo extends StatefulWidget {
+  final DataBase db;
+  GroupInfo({Key? key, required this.db})
+      : super(key: key);
+
+  @override
+  _GroupInfo createState() => _GroupInfo();
+}
+
+
+
+class _GroupInfo extends State<GroupInfo> {
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //header
+       
+   Stack(
+           
+          ),
+
+
+
+
+
+ Stack(
+           
+          ),
+
+
+
+           Stack(
+           
+          ),
+
+
+
+
+          
+        ],
+      ),
+    );
+  }
+  }
